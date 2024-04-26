233771a65ad9fb887cd39b28a0ca40abcb49c5f9
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-04-26 15:31:54 +0100

    - parameterised test so we can check more speeds

--- src/obd2NET.Tests/Vehicle_Tests.cs
+++ src/obd2NET.Tests/Vehicle_Tests.cs
@@ -14,9 +14,10 @@ public sealed class Vehicle_Tests
   }
 
   [Test]
-  public void Speed_returns_expected()
+  public void Speed_returns_expected([Values(50u, 150u, 30u, 221u)] uint exp)
   {
-    var dataStr = "\n01 0d 32 \r\n>";
+    var expStr = exp.ToString("x2");
+    var dataStr = $"\n01 0d {expStr} \r\n>";
     var data = new List<byte>(Encoding.Default.GetBytes(dataStr));
 
     _port.Setup(x => x.Read(It.IsAny<byte[]>(), 0, 1024))
@@ -24,6 +25,6 @@ public sealed class Vehicle_Tests
 
     var res = Vehicle.Speed(_serConn);
 
-    res.Should().Be(50);
+    res.Should().Be(exp);
   }
 }

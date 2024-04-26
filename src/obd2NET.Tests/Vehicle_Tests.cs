bcb46d15f934e57a5b2b46153a0ad6f44e9d31e5
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-04-26 15:56:29 +0100

    - RPM unit test

--- src/obd2NET.Tests/Vehicle_Tests.cs
+++ src/obd2NET.Tests/Vehicle_Tests.cs
@@ -27,4 +27,22 @@ public sealed class Vehicle_Tests
 
     res.Should().Be(exp);
   }
+
+  [Test]
+  public void RPM_returns_expected([Values(1500u, 1000u, 3500u, 12100u)] uint exp)
+  {
+    var rpmA = exp / 64;
+    var rpmB = exp * 4 - 256 * rpmA;
+    var rpmAstr = rpmA.ToString("x2");
+    var rpmBstr = rpmB.ToString("x2");
+    var dataStr = $"\n01 0c {rpmAstr} {rpmBstr} \r\n>";
+    var data = new List<byte>(Encoding.Default.GetBytes(dataStr));
+
+    _port.Setup(x => x.Read(It.IsAny<byte[]>(), 0, 1024))
+      .Callback<byte[], int, int>((buffer, offset, count) => { data.CopyTo(buffer, 0); });
+
+    var res = Vehicle.RPM(_serConn);
+
+    res.Should().Be(exp);
+  }
 }
